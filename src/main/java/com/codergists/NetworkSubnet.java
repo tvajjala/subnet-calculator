b55fe9a1ac9efabb6aa95c88221e16ce65fe7c65
Thiru Vajjala <thiru.vajjala@oracle.com>
2024-02-20 09:12:11 -0600

    Support wider subnets

--- src/main/java/com/codergists/NetworkSubnet.java
+++ src/main/java/com/codergists/NetworkSubnet.java
@@ -45,17 +45,27 @@ public class NetworkSubnet {
     /**
      * Mask to split CIDR range
      */
-    private Integer subnetMask;
+    private Mask subnetMask;
+
 
     /**
      * @param masterBlockIPV4 masterBlockIPV4
      * @param masterBlockMask masterBlockMask
      * @param subnetMask      subnetMask
      */
-    private NetworkSubnet(String masterBlockIPV4, Integer masterBlockMask, Integer subnetMask) {
+    private NetworkSubnet(String masterBlockIPV4, Integer masterBlockMask, Mask subnetMask) {
         this.masterBlockIPV4 = masterBlockIPV4;
         this.masterBlockMask = masterBlockMask;
         this.subnetMask = subnetMask;
+    }
+
+    /**
+     * Max subnets allowed
+     *
+     * @return max subnets allowed
+     */
+    public int maxSubnetsAllowed() {
+        return Double.valueOf(Math.pow(2, (subnetMask.getValue() - masterBlockMask))).intValue();
     }
 
     /**
@@ -79,41 +89,56 @@ public class NetworkSubnet {
     }
 
     public Iterator<String> getSubnetCidrBlocks() {
-        return new CIDRBlockHolder(masterBlockIPV4, subnetMask);
+
+        return new CIDRBlockHolder(masterBlockIPV4, subnetMask, maxSubnetsAllowed());
     }
 
     class CIDRBlockHolder implements Iterator<String> {
 
         private int[] blocks;
-        private int maxIpCount;
-
+        /**
+         * subnet block ip range required
+         */
+       // private int maxIpCount;
+        Mask subnetMask;
         private int b0;
         private int b1;
         private int b2;
         private int b3;
         private boolean first;
+        private int maxSubnetsAllowed;
 
-        CIDRBlockHolder(String startingIp, int splitMask) {
+        CIDRBlockHolder(String startingIp, Mask subnetMask, int maxSubnetsAllowed) {
             this.blocks = stream(startingIp.split("\\.")).mapToInt(Integer::parseInt).toArray();
             b0 = blocks[0];
             b1 = blocks[1];
             b2 = blocks[2];
             b3 = blocks[3];
             first = true;
-            this.maxIpCount = (int) Math.pow(2, (IPV4_MAX - splitMask));
+          //  this.maxIpCount = (int) Math.pow(2, (IPV4_MAX - subnetMask.getValue()));
+            this.subnetMask=subnetMask;
+            this.maxSubnetsAllowed = maxSubnetsAllowed;
         }
 
         boolean hasNextInvoked = false;
+        int subnetCounter = 0;
 
         @Override
         public boolean hasNext() {
+            /* only  2 ^(subnetMask-vcnMask) subnets allowed */
+            if (subnetCounter >= maxSubnetsAllowed) {
+                log.warn("Max allowed subnets exceeded");
+                return false;
+            }
+            subnetCounter +=1;
             hasNextInvoked = true;
+            /* first time just return true, blocks always exists*/
             if (first) {
                 first = false;
                 return true;
             }
-            increment();
 
+            increment();
             //return b0 <= MAX;// stop if 255.255.255.x
             return b2 <= MAX; // stop if x.x.255.x
         }
@@ -124,21 +149,36 @@ public class NetworkSubnet {
                 throw new IllegalStateException("Invalid state - must invoke hasNext before calling next");
             }
             hasNextInvoked = false;
-            String subnet = String.format("%d.%d.%d.%d/%d", b0, b1, b2, b3, subnetMask);
+            String subnet = String.format("%d.%d.%d.%d/%d", b0, b1, b2, b3, subnetMask.getValue());
             log.info("Initial IP " + String.format("%d.%d.%d.%d", b0, b1, b2, b3));
-            log.info("Total IPs " + ipCount(subnetMask));
-            log.info("Full IPList " + getIPList(String.format("%d.%d.%d.%d", b0, b1, b2, b3), subnetMask));
+            log.info("Total IPs " + ipCount(subnetMask.getValue()));
+            log.info("Full IPList " + getFullIPList());
             return subnet;
         }
 
 
         private void increment() {
-            //TODO: bit shifting
-            if ((b3 + maxIpCount) >= MAX) { //0+255, next b3 will be 0, and b2++
-                b3 = 0;
-                b2++;
-            } else {
-                b3 = (b3 + maxIpCount);
+            //TODO: more optimise with bit shifting
+            if (subnetMask == Mask._20) {
+                b2 += 16;
+            }else if (subnetMask == Mask._21) {
+                b2 += 8;
+            }else if (subnetMask == Mask._22) {
+                b2 += 4;
+            }else if (subnetMask == Mask._23) {
+                b2 += 2;
+            }else if(subnetMask == Mask._24){
+                b2 += 1;
+            }else if (subnetMask.getValue() < 256) {
+               int  maxIpCount = (int) Math.pow(2, (IPV4_MAX - subnetMask.getValue()));
+                if ((b3 + maxIpCount) >= MAX) { //0+255, next b3 will be 0, and b2++
+                    b3 = 0;
+                    b2++;
+                } else {
+                    b3 = (b3 + maxIpCount);
+                }
+            }else{
+                throw new UnsupportedOperationException("Subnet not supported");
             }
 
          /*   if (b2 >= (MAX - 1)) {
@@ -194,7 +234,7 @@ public class NetworkSubnet {
     /**
      * IP Count for the given mask
      *
-     * @param subnetMask
+     * @param subnetMask subnetMask
      * @return total IP Count
      */
     private static int ipCount(int subnetMask) {
@@ -225,7 +265,8 @@ public class NetworkSubnet {
 
         private Protocol protocol;
 
-        private Integer subnetMask;
+        private Mask subnetMask;
+
 
         public Builder(Protocol protocol) {
 
@@ -244,7 +285,7 @@ public class NetworkSubnet {
         }
 
         Builder withSubnetMask(Mask subnetMask) {
-            this.subnetMask = subnetMask.getValue();
+            this.subnetMask = subnetMask;
             return this;
         }
 
@@ -257,12 +298,13 @@ public class NetworkSubnet {
             }
 
             // Must be within (32 > subnetMask > masterBlockMask)
-            if ((subnetMask > 32) || (masterBlockMask > subnetMask)) {
+            if ((subnetMask.getValue() > 32) || (masterBlockMask > subnetMask.getValue())) {
                 throw new IllegalArgumentException("Invalid subnetMask value provided");
             }
 
             return new NetworkSubnet(masterBlockIPv4, masterBlockMask, subnetMask);
         }
+
 
         private boolean isValidIPV4(String ipv4) {
             if (ipv4.split("\\.").length != 4) {
@@ -279,18 +321,28 @@ public class NetworkSubnet {
         /**
          * Only Supports IPV4
          */
-        IPV4;
-
-        // IPV6;//FIXME: Doesn't support
+        IPV4,
+        /**
+         * IPV6 Supports not yet implemented
+         */
+        IPV6;
 
         public boolean isIPv4() {
             return this == IPV4;
         }
 
+        public boolean isIPv6() {
+            return this == IPV6;
+        }
+
     }
 
     enum Mask {
+        _20(20, 4096),
+        _21(21, 2048),
 
+        _22(22, 1024),
+        _23(23, 512),
         _24(24, 256),
         _25(25, 128),
         _26(26, 64),
--- src/test/java/com/codergists/CidrBlockBuilderTest.java
+++ src/test/java/com/codergists/CidrBlockBuilderTest.java
@@ -1,14 +1,61 @@
 package com.codergists;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
 
 /**
  * @author tvajjala
+ *
+ * https://www.site24x7.com/tools/ipv4-subnetcalculator.html
  */
- class CidrBlockBuilderTest {
+class CidrBlockBuilderTest {
+
+    @Test
+    void subnetBlockWithMask20Test() {
+
+        //given:
+        NetworkSubnet networkSubnet = NetworkSubnet.builder(NetworkSubnet.Protocol.IPV4)
+                .fromMasterBlock("10.0.128.0/17")//<-- vcnSubnet
+                .withSubnetMask(NetworkSubnet.Mask._20)
+                .build();
+
+        //assuming max subnets allowed are 8
+        Assumptions.assumeTrue(8 == networkSubnet.maxSubnetsAllowed());
+
+        //when:
+        Iterator<String> iterator = networkSubnet.getSubnetCidrBlocks();
+
+        //then: with 256 IP blocks
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.128.0/20", iterator.next());
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.144.0/20", iterator.next());
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.160.0/20", iterator.next());
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.176.0/20", iterator.next());
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.192.0/20", iterator.next());
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.208.0/20", iterator.next());
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.224.0/20", iterator.next());
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.240.0/20", iterator.next());
+
+        // max subnets reached for given vcn range
+        Assumptions.assumeFalse(iterator.hasNext());
+    }
 
     @Test
     void subnetBlockWithMask24Test() {
@@ -18,6 +65,9 @@ import java.util.Iterator;
                 .fromMasterBlock("10.0.128.0/17")//<-- vcnSubnet
                 .withSubnetMask(NetworkSubnet.Mask._24)
                 .build();
+
+        //assuming max subnets allowed are 32
+        Assumptions.assumeTrue(128 == networkSubnet.maxSubnetsAllowed());
 
         //when:
         Iterator<String> iterator = networkSubnet.getSubnetCidrBlocks();
@@ -31,6 +81,38 @@ import java.util.Iterator;
 
         Assertions.assertTrue(iterator.hasNext());
         Assertions.assertEquals("10.0.130.0/24", iterator.next());
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.131.0/24", iterator.next());
+    }
+
+    @Test
+    void subnetBlockWithMask22Test() {
+
+        //given:
+        NetworkSubnet networkSubnet = NetworkSubnet.builder(NetworkSubnet.Protocol.IPV4)
+                .fromMasterBlock("10.0.128.0/17") //<-- vcnSubnet
+                .withSubnetMask(NetworkSubnet.Mask._22)
+                .build();
+
+        //assuming max subnets allowed are 32
+        Assumptions.assumeTrue(32 == networkSubnet.maxSubnetsAllowed());
+
+        //when:
+        Iterator<String> iterator = networkSubnet.getSubnetCidrBlocks();
+
+        //then: with 1024 IP blocks
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.128.0/22", iterator.next());
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.132.0/22", iterator.next());
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.136.0/22", iterator.next());
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("10.0.140.0/22", iterator.next());
 
     }
 
@@ -56,6 +138,7 @@ import java.util.Iterator;
 
         Assertions.assertTrue(iterator.hasNext());
         Assertions.assertEquals("10.0.128.32/28", iterator.next());
+
 
     }
 
